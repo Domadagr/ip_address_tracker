bdac927647d17bb5e3d2ae32371e32903a9039ba
Domadagr <dev@telemetri.xyz>
2024-11-25 13:29:17 +0100

    basic layout test

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// initial 
-const searchForm = document.querySelector('.headerForm');
+const searchForm = document.querySelector('.header_form');
 
 searchForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    const value = document.querySelector('#search').value;
+    const value = document.getElementById('search').value;
     console.log(value);
+    // ... your code to fetch and process the IP address ...
 });
