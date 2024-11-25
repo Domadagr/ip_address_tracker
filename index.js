18ccc23152d081c971876f3a76f6e37a5356edd6
Domadagr <dev@telemetri.xyz>
2024-11-25 10:13:40 +0100

    added initial js, html, css

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // initial 
+const searchForm = document.querySelector('.headerForm');
+
+searchForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const value = document.querySelector('#search').value;
+    console.log(value);
+});
